3fe8dfe3c17a1173369f76e86b59104ad0de4365
zotello <zotello@yandex.ru>
2016-03-21 09:03:24 +0300

    initialisation

---
+++ Debug/src/prog_N_1.d
@@ -0,0 +1,1 @@
+src/prog_N_1.o: ../src/prog_N_1.cpp
---
+++ src/prog_N_1.cpp
@@ -0,0 +1,24 @@
+//============================================================================
+// Name        : prog_N_1.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main() {
+	int a = 0;
+	int b = 0;
+	cout << "Введите целое число №1: ";
+	cin >> a;
+	cout <<endl << "Введите целое число №2: ";
+	cin >> b;
+	cout <<endl <<"Произведение введенных чисел = :" << a * b <<endl;
+	cin >>a;
+
+	return 0;
+}
